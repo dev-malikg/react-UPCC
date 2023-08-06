9fe8ead7ace06d57d9df6be7385cac66e312e3e6
GMalik <gmalik@Manojs-MacBook-Air.local>
2023-08-06 09:58:54 +0530

    designing with tailwind compleated

--- src/components/Formtext.js
+++ src/components/Formtext.js
@@ -1,29 +1,82 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 export default function Formtext(props) {
 
-  const [text, setText] = useState("enter text here");
-  const [updatedText, setUpdatedText] = useState("Your updted text will appear here :)");
+  const [text, setText] = useState("");
+  const [updatedText, setUpdatedText] = useState("");
 
-  const handelTextChange = (event)=> {
+  const handelTextChange = (event) => {
     setText(event.target.value); // very imp code 
   }
 
-  const handelReFormat = ()=> {
+  const handelReFormat = () => {
     setUpdatedText(text.toUpperCase())
   }
 
   return (
-    <div className='textt'>
-      <div>
-        <p>Enter a text below to get started</p>
-        <textarea cols="30" rows={props.lines} className='texttarea' value={text} onChange={handelTextChange}></textarea>
+    <>
 
-        <br/> <button onClick={handelReFormat}>ReFormet</button>
-        <p className='texttarea' >{updatedText}</p>
-      </div>
-    </div>
+      <section className="text-gray-600 body-font relative">
+        <div className="container px-5 py-10 mx-auto ">
+          <div className="flex flex-col text-center w-full mb-10">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Text Changer</h1>
+          </div>
+          <div className="lg:w-1/2 md:w-2/3 mx-auto">
+            <div className="flex flex-wrap -m-2">
+              
+              <div className="p-2 w-full">
+
+                <div className="relative">
+                  <label for="message" className="leading-7 text-sm text-gray-600">Your Message</label>
+                  <textarea id="message" name="message" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" value={text} onChange={handelTextChange}></textarea>
+                </div>
+
+                <div className="relative">
+                  <label for="message" className="leading-7 text-sm text-gray-600">Output</label>
+                  <textarea id="message" name="message" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 h-20 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" disabled={true} value={updatedText}></textarea>
+                </div>
+                
+              </div>
+
+              <div className="p-2 w-full">
+                <button onClick={handelReFormat} className="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">ReFormat</button>
+              </div>
+
+              <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
+                <a className="text-green-500" href='/'>gaurav.arr@gmail.com</a>
+                <p className="leading-normal my-5">49 Smith St.
+                  <br/>Saint Cloud, MN 56301
+                </p>
+                <span className="inline-flex">
+                  <a className="text-gray-500" href='/'>
+                    <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                      <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                    </svg>
+                  </a>
+                  <a className="ml-4 text-gray-500" href='/'>
+                    <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                      <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                    </svg>
+                  </a>
+                  <a className="ml-4 text-gray-500" href='/'>
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                      <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                      <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                    </svg>
+                  </a>
+                  <a className="ml-4 text-gray-500" href='/'>
+                    <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                    </svg>
+                  </a>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,17 +3,40 @@ import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
   return (
-    <nav className='nav_bar'>
-        <li className='bars' ><span className='pagename'>{props.title}</span></li>
-        <li className='bars' > home </li>
-        <li className='bars' > about </li>
-        <li className='bars' > blogs </li>
-    </nav>
+    <header className="text-gray-600 body-font">
+      <div className="flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" href='/'>
+          <span className="ml-3 text-xl">GPower</span>
+        </a>
+        <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+          <a className="mr-5 hover:text-gray-900" href='/'>Home</a>
+          <a className="mr-5 hover:text-gray-900" href='/'>About</a>
+          <a className="mr-5 hover:text-gray-900" href='/'>Info</a>
+        </nav>
+
+
+        <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Search
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </button>
+
+        {/* <select data-te-select-init>
+          <option value="1">One</option>
+          <option value="2" disabled>Two</option>
+          <option value="3" disabled>Three</option>
+          <option value="4">Four</option>
+          <option value="5">Five</option>
+        </select> */}
+
+
+      </div>
+    </header>
   )
 }
 
 // galti pacadna ka lia :)
-Navbar.propTypes = { 
-    title: PropTypes.string.isRequired, 
-    about: PropTypes.string
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  about: PropTypes.string
 }
